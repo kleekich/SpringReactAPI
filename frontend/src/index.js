a160504cd17d20957024b5766f121ffc88a580e8
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-08-09 22:09:32 -0700

    Add React Router DOM for CRUD Front End

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './App.css';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Show from './components/Show';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/edit/:id' component={Edit} />
+        <Route path='/create' component={Create} />
+        <Route path='/show/:id' component={Show} />
+      </div>
+  </Router>,
+  document.getElementById('root')
+);
